732136b6544337ac01eb778247c56ec244537ee0
AHMAD ROFIKIL KHOIRI <ahmadrofikil46@gmail.com>
2020-04-09 20:31:07 +0700

    menambahkan data film pada model

--- UTSku/app/src/main/java/com/example/uts_ku/model/DataFilm.java
+++ UTSku/app/src/main/java/com/example/uts_ku/model/DataFilm.java
@@ -17,7 +17,9 @@ public class DataFilm {
 
             {"DILAN 1990 (2018)",
                     "Film Drama",
-                    "Dilan 1990 merupakan film drama Indonesia tahun 2018.Film ini diangkat dari novel Dilan: Dia adalah Dilanku Tahun 1990 karya Pidi Baiq dan dibintangi oleh Iqbaal Ramadhan dan Vanesha Prescilla.[2][3] Para pemain pendukungnya antara lain Farhan, Ira Wibowo, Tike Priatnakusumah, dan personel grup idola JKT48, Adhisty Zara. Ridwan Kamil, yang saat rilis film menjabat sebagai Wali Kota Bandung, juga ikut bermain di film ini. " +
+                    "Dilan 1990 merupakan film drama Indonesia tahun 2018.Film ini diangkat dari novel Dilan: Dia adalah Dilanku Tahun 1990 karya Pidi Baiq dan dibintangi oleh Iqbaal Ramadhan dan Vanesha Prescilla. " +
+                            "Para pemain pendukungnya antara lain Farhan, Ira Wibowo, Tike Priatnakusumah, dan personel grup idola JKT48, Adhisty Zara. " +
+                            "Ridwan Kamil, yang saat rilis film menjabat sebagai Wali Kota Bandung, juga ikut bermain di film ini. " +
                             "Kakak Vanesha, Sissy Priscillia, menjadi narator film sekaligus suara dari Milea dewasa. ",
                     "https://cdn.cgv.id/uploads/movie/compressed/17009800.jpg",
                     "Iqbaal Ramadhan, Vanesha Prescilla," +"\n"+
@@ -38,34 +40,67 @@ public class DataFilm {
 
             {"DANUR (2018)",
                     "Film Horor",
-                    "Danur: I Can See Ghosts) merupakan film horor Indonesia yang dirilis pada 30 Maret 2017 disutradarai oleh Awi Suryadi. Film ini diangkat dari buku karya Risa Saraswati, Gerbang Dialog Danur yang ditulis berdasarkan pengalamannya sendiri sebagai seorang gadis indigo. Film ini dibintangi oleh Prilly Latuconsina, Sandrinna Michelle, Shareefa Daanish, Indra Brotolaras, dan Kinaryosih serta memiliki jumlah penonton sebanyak 2.736.391 orang.",
+                    "Danur: I Can See Ghosts) merupakan film horor Indonesia yang dirilis pada 30 Maret 2017 disutradarai oleh Awi Suryadi. " +
+                            "Film ini diangkat dari buku karya Risa Saraswati, Gerbang Dialog Danur yang ditulis berdasarkan pengalamannya sendiri sebagai seorang gadis indigo. " +
+                            "Film ini dibintangi oleh Prilly Latuconsina, Sandrinna Michelle, Shareefa Daanish, Indra Brotolaras, dan Kinaryosih serta memiliki jumlah penonton sebanyak 2.736.391 orang.",
                     "https://upload.wikimedia.org/wikipedia/id/0/09/Danur_poster.jpg",
                     "Prilly Latuconsina, Sandrinna Michelle, Shareefa " +"\n"+
                             "Daanish, Kinaryosih, Indra Brotolaras, Inggrid " +"\n"+
                             "Widjanarko, Zummiyanuran Habibi"},
 
+            {"Bumi Manusia (2019)",
+                    "Film Drama",
+                    "Bumi Manusia merupakan film drama biografi sejarah Indonesia tahun 2019 yang disutradarai Hanung Bramantyo dan ditulis Salman Aristo. " +
+                            "Film ini dialihwahanakan dari novel berjudul sama karya Pramoedya Ananta Toer. Film ini dibintangi Iqbaal Ramadhan, Mawar Eva de Jongh, dan Sha Ine Febriyanti. " +
+                            "Film ini menceritakan kegamangan Minke antara kemajuan Eropa dan perjuangan membela tanah airnya serta hubungannya dengan Annelies..",
+                    "https://upload.wikimedia.org/wikipedia/id/5/51/Bumi_Manusia_poster.jpg",
+                    "Iqbaal Ramadhan, Mawar Eva de Jongh, Sha Ine Febriyanti\n" +
+                            "Ayu Laksmi, Donny Damara, Bryan Domani\n" +
+                            "Giorgino Abraham" +
+                            "Jerome Kurniawan"},
+
             {"Aquaman (2018)",
             "Film Action",
             "Aquaman adalah film pahlawan super Amerika tahun 2018 yang didasarkan pada karakter DC Comics, Aquaman, dan didistribusikan oleh Warner Bros. " +
                     "Pictures. Ini adalah angsuran keenam di DC Extended Universe (DCEU). Film ini disutradarai oleh James Wan, dengan skenario oleh David Leslie Johnson-McGoldrick dan Will Beall, dan didasarkan pada sebuah cerita dari Geoff Johns, Wan dan Beall. " +
-                    "Ini dibintangi Jason Momoa sebagai karakter judul, dengan Amber Heard, Willem Dafoe, Patrick Wilson, Dolph Lundgren, Yahya Abdul-Mateen II, dan Nicole Kidman dalam peran pendukung. Ini adalah film teater laga hidup ketiga yang menampilkan Aquaman.",
+                    "Ini dibintangi Jason Momoa sebagai karakter judul, dengan Amber Heard, Willem Dafoe, Patrick Wilson, Dolph Lundgren, Yahya Abdul-Mateen II, dan Nicole Kidman dalam peran pendukung. " +
+                    "Ini adalah film teater laga hidup ketiga yang menampilkan Aquaman.",
             "https://upload.wikimedia.org/wikipedia/id/3/3a/Aquaman_poster.jpg",
             "Jason Momoa, Amber Heard, Willem Dafoe, "+"\n"+
                     "Patrick Wilson, Dolph Lundgren, " +"\n"+
                     "Yahya Abdul-Mateen II, Nicole Kidman"},
 
             {"Captain Marvel (2019)",
-                    "Film Action",
-                    "Captain Marvel adalah film superhero Amerika Serikat tahun 2019 yang disutradarai oleh Anna Boden dan Ryan Fleck dan diproduseri oleh Kevin Feige. " +
-                            "Ini adalah film kedua puluh satu dari seri Marvel Cinematic Universe." +
-                            " Naskah film ini ditulis oleh Anna Boden, Lili Qadriana, Ryan Fleck, Geneva Robertson-Dworet dan Jac Schaeffer, sementara ceritanya ditulis oleh Nicole Perlman, " +
-                            "Joe Schrapnel dan Anna Waterhouse berdasarkan komik Carol Danvers karya Roy Thomas dan Gene Colan. Film ini dibintangi oleh Brie Larson, Samuel L. Jackson, Ben Mendelsohn, " +
-                            "Djimon Hounsou, Lee Pace, Lashana Lynch, Gemma Chan, Annette Bening, Clark Gregg dan Jude Law.",
-                    "https://upload.wikimedia.org/wikipedia/id/d/d0/CaptainMarvelPoster2019.jpg",
-                    " Brie Larson, Samuel L. Jackson, Ben Mendelsohn, "+"\n"
+            "Film Action",
+            "Captain Marvel adalah film superhero Amerika Serikat tahun 2019 yang disutradarai oleh Anna Boden dan Ryan Fleck dan diproduseri oleh Kevin Feige. " +
+                    "Ini adalah film kedua puluh satu dari seri Marvel Cinematic Universe." +
+                    " Naskah film ini ditulis oleh Anna Boden, Lili Qadriana, Ryan Fleck, Geneva Robertson-Dworet dan Jac Schaeffer, sementara ceritanya ditulis oleh Nicole Perlman, " +
+                    "Joe Schrapnel dan Anna Waterhouse berdasarkan komik Carol Danvers karya Roy Thomas dan Gene Colan. Film ini dibintangi oleh Brie Larson, Samuel L. Jackson, Ben Mendelsohn, " +
+                    "Djimon Hounsou, Lee Pace, Lashana Lynch, Gemma Chan, Annette Bening, Clark Gregg dan Jude Law.",
+            "https://upload.wikimedia.org/wikipedia/id/d/d0/CaptainMarvelPoster2019.jpg",
+            " Brie Larson, Samuel L. Jackson, Ben Mendelsohn, "+"\n"
                     + "Djimon Hounsou, Lee Pace, Lashana Lynch, " + "\n" +
-                            "Gemma Chan,Annette Bening, Clark Gregg, Jude Law"},
-    };
+                    "Gemma Chan,Annette Bening, Clark Gregg, Jude Law"},
+
+            {"Gundala (2019)",
+                    "Film Action",
+                    "Gundala adalah sebuah film pahlawan super neo-noir Indonesia tahun 2019 yang disutradarai dan ditulis oleh Joko Anwar. " +
+                            "Film ini adalah produksi bersama Screenplay Films, Legacy Pictures, Ideosource Entertainment, dengan pemilik hak cipta Gundala yaitu Bumilangit Studios. " +
+                            "Film ini berdasarkan pada cerita karakter pahlawan super Indonesia tahun 1969 Gundala yang dibuat oleh Harya Suraminata. Karakter utamanya sendiri diperankan oleh Abimana Aryasatya. " +
+                            "Film ini akan menjadi awal dari Jagat Sinema Bumilangit (JSB). .",
+                    "https://upload.wikimedia.org/wikipedia/id/d/de/Gundala_%282019%29_poster.jpg",
+                    "Abimana Aryasatya, Tara Basro, Bront Palarae, Ario Bayu\n" +
+                            "Rio Dewanto, Marissa Anita"},
+
+            {"The Call of the Wild (2020)",
+                    "Film Action",
+                    "The Call of the Wild is a 2020 American adventure film based on the Jack London 1903 novel of the same name and on Twentieth Century Pictures' 1935 film adaptation. " +
+                            "Directed by Chris Sanders, in his live-action directorial debut, the film was written by Michael Green, and stars Harrison Ford, Dan Stevens, Omar Sy, Karen Gillan, Bradley Whitford, and Colin Woodell." +
+                            " Set during the 1890s Klondike Gold Rush, the film follows a dog named Buck as he is stolen from his home in California and sent to the Yukon, where he befriends an old outdoorsman and begins a life-altering adventure. .",
+                    "https://upload.wikimedia.org/wikipedia/en/4/43/The_Call_of_the_Wild_poster.jpg",
+                    "Harrison Ford Omar Sy, Dan Stevens, Karen Gillan\n" +
+                            "Bradley Whitford "},
+};
 
     public static ArrayList<Film> getListData() {
         Film bioskop;
